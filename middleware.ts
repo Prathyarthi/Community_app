967e6e23858cc0e9fdf352819d859ebfb8e86d94
Prathyarthi <prathyarti@gmail.com>
2024-06-09 11:11:27 +0530

    Stripe instance

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,13 @@ const protectedRoutes = createRouteMatcher([
   "/"
 ])
 
+const publicRoutes = createRouteMatcher([
+  '/api/webhook'
+])
+
 export default clerkMiddleware((auth, req) => {
   if (protectedRoutes(req)) auth().protect()
+  if (publicRoutes(req)) auth()
 });
 
 export const config = {
